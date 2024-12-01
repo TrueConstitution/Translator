8dec708e8f3c447ff33ee5bef9f2eefd839c6fc7
KGG_XK <1838926478@qq.com>
2024-12-01 15:34:27 +0800

    更新游戏版本到1.20.4

--- src/main/java/kgg/translator/mixin/hud/InGameHudForTitleMixin.java
+++ src/main/java/kgg/translator/mixin/hud/InGameHudForTitleMixin.java
@@ -11,26 +11,14 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudForTitleMixin {
-    @ModifyArg(method = "renderTitleAndSubtitle", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;getWidth(Lnet/minecraft/text/StringVisitable;)I"), index = 0)
-    public StringVisitable getWidthTitle(StringVisitable text) {
+    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;getWidth(Lnet/minecraft/text/StringVisitable;)I"), index = 0)
+    public StringVisitable getWidth(StringVisitable text) {
         if (!ScreenOption.autoTitle.isEnable()) return text;
         return TranslateHelper.translateNoWait((Text) text);
     }
 
-    @ModifyArg(method = "renderTitleAndSubtitle", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithBackground(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIII)I"), index = 1)
-    public Text renderTitle(Text text) {
-        if (!ScreenOption.autoTitle.isEnable()) return text;
-        return TranslateHelper.translateNoWait(text);
-    }
-
-    @ModifyArg(method = "renderOverlayMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;getWidth(Lnet/minecraft/text/StringVisitable;)I"), index = 0)
-    public StringVisitable getWidthOverlay(StringVisitable text) {
-        if (!ScreenOption.autoTitle.isEnable()) return text;
-        return TranslateHelper.translateNoWait((Text) text);
-    }
-
-    @ModifyArg(method = "renderOverlayMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithBackground(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIII)I"), index = 1)
-    public Text renderOverlay(Text text) {
+    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I"), index = 1)
+    public Text render(Text text) {
         if (!ScreenOption.autoTitle.isEnable()) return text;
         return TranslateHelper.translateNoWait(text);
     }
--- src/main/java/kgg/translator/screen/OptionsScreen.java
+++ src/main/java/kgg/translator/screen/OptionsScreen.java
@@ -5,14 +5,26 @@ import kgg.translator.option.ScreenOption;
 import kgg.translator.option.WorldOption;
 import kgg.translator.option.ChatOption;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.screen.option.SimpleOptionsScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.Text;
 
-public class OptionsScreen extends GameOptionsScreen {
+public class OptionsScreen extends SimpleOptionsScreen {
     public OptionsScreen() {
-        super(null, MinecraftClient.getInstance().options, Text.literal("翻译选项"));
+        super(null, MinecraftClient.getInstance().options, Text.literal("翻译选项"),
+                new SimpleOption[]{
+                        createOption(ChatOption.autoChat),
+                        createOption(ChatOption.chatTip),
+                        createOptionAndTooltip(ScreenOption.autoTitle),
+                        createOption(ScreenOption.autoScoreboard),
+                        createOption(ScreenOption.autoBossBar),
+                        createOptionAndTooltip(ScreenOption.autoTooltip),
+                        createOption(WorldOption.autoEntityName),
+                        createOption(WorldOption.autoSign),
+                        createOptionAndTooltip(ScreenOption.screenTranslate),
+                        createOptionAndTooltip(WorldOption.signCombine)
+                });
     }
 
     private static SimpleOption<Boolean> createOption(Option option) {
@@ -23,19 +35,5 @@ public class OptionsScreen extends GameOptionsScreen {
         return SimpleOption.ofBoolean("translator.option." + option.name,
                 (b) -> Tooltip.of(Text.translatable("translator.option." + option.name + ".desc")),
                 option.isEnable(), option::setEnable);
-    }
-
-    @Override
-    protected void addOptions() {
-        this.body.addAll(createOption(ChatOption.autoChat),
-                createOption(ChatOption.chatTip),
-                createOptionAndTooltip(ScreenOption.autoTitle),
-                createOption(ScreenOption.autoScoreboard),
-                createOption(ScreenOption.autoBossBar),
-                createOptionAndTooltip(ScreenOption.autoTooltip),
-                createOption(WorldOption.autoEntityName),
-                createOption(WorldOption.autoSign),
-                createOptionAndTooltip(ScreenOption.screenTranslate),
-                createOptionAndTooltip(WorldOption.signCombine));
     }
 }
